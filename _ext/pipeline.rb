382eff2c4c2174eaf650b277a7f3c341f5831ad6
pdudits <patrik@dudits.net>
2014-04-08 23:30:19 +0200

    Use relative hrefs on title page and in template

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,6 +4,7 @@ require 'sass_functions'
 Awestruct::Extensions::Pipeline.new do
 
   extension Awestruct::Extensions::Posts.new '/posts'
+  helper Awestruct::Extensions::Relative
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
