eec0948a0d128f0feaff20f965127534ff68cfda
pdudits <patrik@dudits.net>
2014-04-29 00:07:33 +0200

    Adding tagger and a post describing that. Also entry partial.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -5,7 +5,12 @@ Awestruct::Extensions::Pipeline.new do
 
   extension Awestruct::Extensions::Posts.new '/posts'
   helper Awestruct::Extensions::Relative
+  helper Awestruct::Extensions::Partial
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
+  extension Awestruct::Extensions::Tagger.new( :posts,
+                                               '/_layouts/tag/index',
+                                               '/tags',
+                                               :per_page=>10 )
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
 end
