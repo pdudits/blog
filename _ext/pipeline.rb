7979685ec498b2c76ccdc3fa32d1358b5baae427
Patrik Dudits <patrik@dudits.net>
2014-11-12 00:42:37 +0100

    Adding support for paging posts

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,8 +4,10 @@ require 'sass_functions'
 Awestruct::Extensions::Pipeline.new do
 
   extension Awestruct::Extensions::Posts.new '/posts'
+  extension Awestruct::Extensions::Paginator.new( :posts, '/_layouts/posts/index', {:per_page=>10, :output_prefix=>'/posts', :page_name => 'index' })  
   helper Awestruct::Extensions::Relative
   helper Awestruct::Extensions::Partial
+
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Tagger.new( :posts,
