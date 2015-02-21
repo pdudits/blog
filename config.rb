b70e6dee2ba24650cef1ab7dfaba2f3682380c7a
Patrik Dudits <patrik@dudits.net>
2015-02-21 10:44:28 +0100

    Moving from awestruct to jBake step 1

--- config.rb
+++ config.rb
@@ -1,13 +1,12 @@
 require 'zurb-foundation'
-require '_ext/sass_functions'
-# Require any additional compass plugins here.
+require_relative 'src/main/sass/_ext/sass_functions'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "_site/stylesheets"
-sass_dir = "stylesheets"
-images_dir = "images"
-javascripts_dir = "javascripts"
+#http_path = "/"
+#css_dir = "_site/stylesheets"
+#sass_dir = "stylesheets"
+#images_dir = "images"
+#javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
